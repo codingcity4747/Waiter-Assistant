55b83479d239a3878f5b3cf30542c6a25df12c43
ashraf <Ashraf.mohamed4728@gmail.com>
2019-02-23 16:12:51 +0200

    initialing waiterAssitant project with no code

---
+++ app/src/main/java/com/city/coding2/waiterassistant/MainActivity.java
@@ -0,0 +1,14 @@
+package com.city.coding2.waiterassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/city/coding2/waiterassistant/dumData.java
@@ -0,0 +1,17 @@
+package com.city.coding2.waiterassistant;
+
+import java.util.ArrayList;
+
+public class dumData {
+
+
+
+    public ArrayList<table> setAndGetTableInfo(int tableNum){
+        ArrayList<table> tableInfo = new ArrayList<>();
+        for(int i =0 ; i<tableNum ;i++){
+            table t = new table(i+1,i*2,10,true);
+        }
+        return tableInfo;
+    }
+
+}
